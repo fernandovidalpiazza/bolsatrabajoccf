bfa0528cc50bbb01a77c42fd95b13b680d64efe6
fernandovidalpiazza <fervidalpiazza@gmail.com>
2024-06-26 21:39:13 -0300

    home

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,27 +1,47 @@
-import React from "react";
-import conectadoImage from "../../assets/conectado.jpeg";
-import { Box, Typography, Fade } from "@mui/material";
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Box } from '@mui/material';
+import img1 from '../../assets/1.jpeg';
+import img2 from '../../assets/2.jpeg';
+import img3 from '../../assets/3.jpeg';
+import img4 from '../../assets/4.jpeg';
+import conectado from '../../assets/conectado.jpeg';
+import styles from './styles.module.css'; // Corrige la importación de estilos
 
 const Home = () => {
   return (
-    <Box
-      className="home-container"
-      sx={{
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        textAlign: "center",
-        padding: "20px",
-        position: "relative",
-      }}
-    >
-        
-      {/* Imagen */}
-      <img src={conectadoImage} alt="Conectado" style={{ width: "100%", zIndex: 0, position: "relative", marginTop: "20px" }} />
-    </Box >
-    
+    <Box className={styles.homeContainer}>
+      <img src={conectado} alt="Conectado" className={styles.conectadoImage} />
+
+      <Box className={styles.carouselContainer}>
+        <Carousel
+          showArrows={true}
+          showStatus={false}
+          showThumbs={false}
+          infiniteLoop={true}
+          autoPlay={true}
+          interval={5000}
+          transitionTime={500}
+          stopOnHover={true}
+          emulateTouch={true}
+          swipeable={true}
+        >
+          <div>
+            <img src={img1} alt="Slide 1" className={styles.slideImage} />
+          </div>
+          <div>
+            <img src={img2} alt="Slide 2" className={styles.slideImage} />
+          </div>
+          <div>
+            <img src={img3} alt="Slide 3" className={styles.slideImage} />
+          </div>
+          <div>
+            <img src={img4} alt="Slide 4" className={styles.slideImage} />
+          </div>
+        </Carousel>
+      </Box>
+    </Box>
   );
 };
 
