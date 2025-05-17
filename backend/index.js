98303d0de053c004cac5717e5dd27c9b2bb2ec4e
fernandovidalpiazza <fervidalpiazza@gmail.com>
2025-05-17 13:50:10 -0300

    texto mail

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,14 @@ app.post("/sendRegistrationEmails", async (req, res) => {
 
     let adminMail = "";
     let adminMsg = "";
-    const userMsg = `¡Bienvenido/a ${nombre}! Gracias por registrarte en la Bolsa de Trabajo.`;
+    const userMsg = `¡Bienvenido/a ${nombre}!¡Bienvenido/a a la Bolsa de Trabajo de la Coalición Cívica!
+    Nos alegra que estés utilizando esta plataforma. Este espacio fue creado con el objetivo de conectar a personas que buscan empleo con empleadores que necesitan cubrir puestos de trabajo. Creemos en el valor del trabajo digno y en la importancia de fortalecer los vínculos dentro de nuestra comunidad.
+    
+    La Coalición Cívica ofrece esta herramienta de búsqueda laboral como un servicio gratuito y comunitario.
+    
+    ⚠️ Importante: La Coalición Cívica no asume ninguna responsabilidad sobre el proceso de contratación, ni mantiene relación laboral alguna con las partes involucradas (contratante y contratado).
+    
+    Esperamos que esta herramienta te sea útil y te deseamos mucho éxito en tu búsqueda.`;
 
     if (ciudad === "San Nicolás") {
       adminMail = "coalicioncivicasannicolas@hotmail.com";
