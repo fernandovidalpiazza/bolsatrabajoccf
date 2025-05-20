34890556a8d33dad8e64407c6981d0b5c1b82895
fernandovidalpiazza <fervidalpiazza@gmail.com>
2025-05-20 14:22:44 -0300

    san nicolas mail

--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,8 @@ app.post("/sendRegistrationEmails", async (req, res) => {
   try {
     const { email, nombre, ciudad } = req.body;
 
-    let adminMail = "";
-    let adminMsg = "";
+    const adminMail = "ccariramallo@gmail.com";
+    const adminMsg = `Se registró una persona: ${nombre} (${email}) de ${ciudad}`;
     const userMsg = `¡Bienvenido/a ${nombre}!¡Bienvenido/a a la Bolsa de Trabajo de la Coalición Cívica!
     Nos alegra que estés utilizando esta plataforma. Este espacio fue creado con el objetivo de conectar a personas que buscan empleo con empleadores que necesitan cubrir puestos de trabajo. Creemos en el valor del trabajo digno y en la importancia de fortalecer los vínculos dentro de nuestra comunidad.
     
@@ -29,16 +29,6 @@ app.post("/sendRegistrationEmails", async (req, res) => {
     ⚠️ Importante: La Coalición Cívica no asume ninguna responsabilidad sobre el proceso de contratación, ni mantiene relación laboral alguna con las partes involucradas (contratante y contratado).
     
     Esperamos que esta herramienta te sea útil y te deseamos mucho éxito en tu búsqueda.`;
-
-    if (ciudad === "San Nicolás") {
-      adminMail = "coalicioncivicasannicolas@hotmail.com";
-      adminMsg = `Se registró una persona de San Nicolás: ${nombre} (${email})`;
-    } else if (ciudad === "Ramallo") {
-      adminMail = "ccariramallo@gmail.com";
-      adminMsg = `Se registró una persona de Ramallo: ${nombre} (${email})`;
-    } else {
-      return res.status(400).json({ success: false, error: "Ciudad no soportada" });
-    }
 
     await transporter.sendMail({
       from: process.env.MAIL_USER,
